4f62989eb8bb10adbe47c404e01e014848b42cb6
Edwin Groothuis <edwin@mavetju.org>
2018-05-28 22:20:56 +1000

    Improve info

--- xkcd-specials/Click and Drag/ClickanddragInfoController.m
+++ xkcd-specials/Click and Drag/ClickanddragInfoController.m
@@ -19,7 +19,12 @@
     [super viewDidLoad];
 
     self.labelTitle.text = @"XKCD 1110 - CLICK AND DRAG";
-    self.labelUsage.text = @"Just scroll around and discover the world.\nZoom in or out on the slider to see more (or less).\nTap to recenter a position.";
+    self.labelUsage.text = @"Just scroll around and discover the world.\n"
+    "Zoom in or out on the slider to see more (or less).\n"
+    "Pinch to zoom in or out.\n"
+    "Tap to recenter a position.\n"
+    "Double Tap to recenter and zoom in.\n"
+    ;
 }
 
 - (IBAction)closeInfo:(id)sender
